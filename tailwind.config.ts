eb2948e789d65b74427f636ee48353b946c28405
CBSP21371337 <164678630+CBSP21371337@users.noreply.github.com>
2024-03-25 19:03:43 +0100

    Zaktualizowanie Kodu
    -Dodane nowe svg
    -Napisany na nowo navbar.js
    
    Do zrobienia ->
    -Ulepszyć jakoś animacje do theme controllera
    -Dodać przycisk zmiany języka
    -Rozpocząć tworzenie page.tsx (główna strona)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,26 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors:{
+        'buttonGray':'#202124',
+        'thInside': 'rgba(0,0,0,33%)',
+      },
+      keyframes:{
+        btnHover:{
+          '0%': {background: 'none'},
+          '50%, 100%': {'font-size': '1.1', color: '#000'},
+          '100%': {background: "#19B6E1", 'font-weight': 'bold', scale:'1.05'},
+        },
+        btnHoverR:{
+          '100%': {background: 'none'},
+          '0%, 50%': {'font-size': '1.1', color: '#000'},
+          '0%': {background: "#19B6E1", 'font-weight': 'bold', scale:'1.05'},
+        }
+      },
+      animation:{
+        btnHover: 'btnHover 0.35s ease-out forwards',
+        btnHoverR: 'btnHoverR 0.55s ease-out backwards',
+      }
     },
     fontSize: {
       sm: '1.2em',
@@ -45,6 +65,9 @@ const config: Config = {
           '.kalaowimglogo': {
             'content': 'url(/sus.png)',
           },
+          '.burger-dp-menu':{
+            'content': 'url(/burger-menu.svg)',
+          },
         },
         light:{
           "primary": "#C33732",
@@ -65,9 +88,18 @@ const config: Config = {
           '.kalaowimglogo': {
             'content': 'url(/logoblack.png)',
           },
+          '.burger-dp-menu':{
+            'content': 'url(/burger-menuL.svg)',
+          },
         },
       },
     ],
+    base: true, // applies background color and foreground color for root element by default
+    styled: false, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
   },
   plugins: [require("daisyui")],
 };
